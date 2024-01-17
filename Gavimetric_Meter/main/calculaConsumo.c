b6e9b5d56e158816b4ee382c3d60f852eb31a2b3
David <davidsanguinonm@outlook.es>
2024-01-17 21:24:52 +0100

    Implementación del cálculo de consumo con máquina de estados

--- Gavimetric_Meter/main/calculaConsumo.c
+++ Gavimetric_Meter/main/calculaConsumo.c
@@ -2,6 +2,7 @@
  * MÓDULO DE MONITORIZACIÓN DEL DEPÓSITO
  * Objeto cíclico: Cálculo de medidas de consumo
  * Lee el buffer de medidas y calcula el consumo medio por cada pareja
+ * La decisión de calcular o no el consumo se realiza mediante una máquina de estados
  ***********************************************************************************************************/
 
 #include <freertos/FreeRTOS.h>
@@ -12,6 +13,7 @@
 
 #include "myTaskConfig.h"
 #include "bufferCircular.h"
+#include "fsm.h"
 #include "calculaConsumo.h"
 
 /* Etiqueta para depuración */
@@ -71,6 +73,242 @@ void timer_next(int* timer)
 {
     if (timer > 0) --timer;
     ESP_LOGD(TAG, "Tiempo restante: %d", timer);
+}
+
+/***********************************************************************************************************
+ * Funciones de condición de transferencia
+ ***********************************************************************************************************/
+// Nota: actualizar las condiciones de comprobación con las funciones reales
+
+/* ESTADO ORIGEN 1: MEDICIÓN MANUAL DESACTIVADA */
+
+bool monitorizacion_c16 (void *params)
+{
+    bool espera_estabilizacion = 0;
+    bool estado_llenado = 0;
+    bool estado_vaciado = 0;
+    return espera_estabilizacion + estado_llenado + estado_vaciado;
+}
+bool monitorizacion_c11 (void *params)
+{
+    return 0; // Nota: cambiar a return emergencia();
+}
+bool monitorizacion_c12 (void *params)
+{
+    return 0; // Nota: cambiar a return comando_medida_puntual();
+}
+bool monitorizacion_c13 (void *params)
+{
+    return 0; // Nota: cambiar a return comando_medida_continuada();
+}
+bool monitorizacion_c14 (void *params)
+{
+    bool comando_modo_remoto = 0;
+    bool peticion_medida = 0;
+    return comando_modo_remoto + !peticion_medida;
+}
+bool monitorizacion_c15 (void *params)
+{
+    bool comando_modo_remoto = 0;
+    bool peticion_medida = 0;
+    return comando_modo_remoto + peticion_medida;
+}
+
+/* ESTADO ORIGEN 2: MEDICIÓN PUNTUAL A CONSOLA */
+
+bool monitorizacion_c26 (void *params)
+{
+    bool espera_estabilizacion = 0;
+    bool estado_llenado = 0;
+    bool estado_vaciado = 0;
+    return espera_estabilizacion + estado_llenado + estado_vaciado;
+}
+bool monitorizacion_c21 (bool *medida_puntual)
+{
+    bool emergencia = 0;
+    return emergencia + medida_puntual;
+}
+bool monitorizacion_c23 (void *params)
+{
+    return 0; // Nota: cambiar a return comando_medida_continuada();
+}
+bool monitorizacion_c24 (void *params)
+{
+    bool comando_modo_remoto = 0;
+    bool peticion_medida = 0;
+    return comando_modo_remoto + !peticion_medida;
+}
+bool monitorizacion_c25 (void *params)
+{
+    bool comando_modo_remoto = 0;
+    bool peticion_medida = 0;
+    return comando_modo_remoto + peticion_medida;
+}
+
+/* ESTADO ORIGEN 3: MEDICIÓN CONTINUADA A CONSOLA */
+
+bool monitorizacion_c36 (void *params)
+{
+    bool espera_estabilizacion = 0;
+    bool estado_llenado = 0;
+    bool estado_vaciado = 0;
+    return espera_estabilizacion + estado_llenado + estado_vaciado;
+}
+bool monitorizacion_c31 (void *params)
+{
+    return 0; // Nota: cambiar a return emergencia();
+}
+bool monitorizacion_c32 (void *params)
+{
+    return 0; // Nota: cambiar a return comando_medida_puntual();
+}
+bool monitorizacion_c34 (void *params)
+{
+    bool comando_modo_remoto = 0;
+    bool peticion_medida = 0;
+    return comando_modo_remoto + !peticion_medida;
+}
+bool monitorizacion_c35 (void *params)
+{
+    bool comando_modo_remoto = 0;
+    bool peticion_medida = 0;
+    return comando_modo_remoto + peticion_medida;
+}
+
+/* ESTADO ORIGEN 4: MEDICIÓN EN REMOTO DESACTIVADA */
+
+bool monitorizacion_c46 (void *params)
+{
+    bool espera_estabilizacion = 0;
+    bool estado_llenado = 0;
+    bool estado_vaciado = 0;
+    return espera_estabilizacion + estado_llenado + estado_vaciado;
+}
+bool monitorizacion_c41 (void *params)
+{
+    bool emergencia = 0;
+    bool comando_modo_manual = 0;
+    return emergencia + comando_modo_manual;
+}
+bool monitorizacion_c42 (void *params)
+{
+    return 0; // Nota: cambiar a return comando_medida_puntual();
+}
+bool monitorizacion_c43 (void *params)
+{
+    return 0; // Nota: cambiar a return comando_medida_continuada();
+}
+bool monitorizacion_c45 (void *params)
+{
+    bool comando_modo_remoto = 0;
+    bool peticion_medida = 0;
+    return comando_modo_remoto + peticion_medida;
+}
+
+/* ESTADO ORIGEN 5: MEDICIÓN EN REMOTO ACTIVADA */
+
+bool monitorizacion_c56 (void *params)
+{
+    bool espera_estabilizacion = 0;
+    bool estado_llenado = 0;
+    bool estado_vaciado = 0;
+    return espera_estabilizacion + estado_llenado + estado_vaciado;
+}
+bool monitorizacion_c51 (void *params)
+{
+    bool emergencia = 0;
+    bool comando_modo_manual = 0;
+    return emergencia + comando_modo_manual;
+}
+bool monitorizacion_c52 (void *params)
+{
+    return 0; // Nota: cambiar a return comando_medida_puntual();
+}
+bool monitorizacion_c53 (void *params)
+{
+    return 0; // Nota: cambiar a return comando_medida_continuada();
+}
+bool monitorizacion_c54 (void *params)
+{
+    bool comando_modo_remoto = 0;
+    bool peticion_medida = 0;
+    return comando_modo_remoto + !peticion_medida;
+}
+
+/* ESTADO ORIGEN 6: ESPERA DE ESTABILIZACIÓN Y VACIADO/LLENADO */
+
+bool monitorizacion_c62 (int *timer)
+{
+    bool emergencia = 0;
+    bool comando_medida_puntual = 0;
+    return !emergencia * comando_medida_puntual * timer_expired(timer);
+}
+bool monitorizacion_c63 (int *timer)
+{
+    bool emergencia = 0;
+    bool comando_medida_continuada = 0;
+    return !emergencia * comando_medida_continuada * timer_expired(timer);
+}
+bool monitorizacion_c64 (int *timer)
+{
+    bool emergencia = 0;
+    bool comando_modo_remoto = 0;
+    bool peticion_medida = 0;
+    return !emergencia * comando_modo_remoto * !peticion_medida * timer_expired(timer);
+}
+bool monitorizacion_c65 (int *timer)
+{
+    bool emergencia = 0;
+    bool comando_modo_remoto = 0;
+    bool peticion_medida = 0;
+    return !emergencia * comando_modo_remoto * peticion_medida * timer_expired(timer);
+}
+bool monitorizacion_c61 (int *timer)
+{
+    return timer_expired(timer);
+}
+
+/***********************************************************************************************************
+ * Inicialización de la máquina de estados
+ ***********************************************************************************************************/
+
+fsm_t* monitorizacionDeposito_new (bool *medida_puntual, int *timer)
+{
+    static fsm_trans_t monitorizacionDeposito_tt[] = {
+        {  1, monitorizacion_c16, 6, NULL },
+        {  1, monitorizacion_c11, 1, NULL },
+        {  1, monitorizacion_c12, 2, NULL },
+        {  1, monitorizacion_c13, 3, NULL },
+        {  1, monitorizacion_c14, 4, NULL },
+        {  1, monitorizacion_c15, 5, NULL },
+        {  2, monitorizacion_c26, 6, NULL },
+        {  2, monitorizacion_c21, 1, NULL },
+        {  2, monitorizacion_c23, 3, NULL },
+        {  2, monitorizacion_c24, 4, NULL },
+        {  2, monitorizacion_c25, 5, NULL },
+        {  3, monitorizacion_c36, 6, NULL },
+        {  3, monitorizacion_c31, 1, NULL },
+        {  3, monitorizacion_c32, 2, NULL },
+        {  3, monitorizacion_c34, 4, NULL },
+        {  3, monitorizacion_c35, 5, NULL },
+        {  4, monitorizacion_c46, 6, NULL },
+        {  4, monitorizacion_c41, 1, NULL },
+        {  4, monitorizacion_c42, 2, NULL },
+        {  4, monitorizacion_c43, 3, NULL },
+        {  4, monitorizacion_c45, 5, NULL },
+        {  5, monitorizacion_c56, 6, NULL },
+        {  5, monitorizacion_c51, 1, NULL },
+        {  5, monitorizacion_c52, 2, NULL },
+        {  5, monitorizacion_c53, 3, NULL },
+        {  5, monitorizacion_c54, 4, NULL },
+        {  6, monitorizacion_c62, 2, NULL },
+        {  6, monitorizacion_c63, 3, NULL },
+        {  6, monitorizacion_c64, 4, NULL },
+        {  6, monitorizacion_c65, 5, NULL },
+        {  6, monitorizacion_c61, 1, NULL },
+        { -1, NULL, -1, NULL },
+    };
+    return fsm_new (monitorizacionDeposito_tt);
 }
 
 /***********************************************************************************************************
